01cbddc76b05f134fb91f59ca18188d9bb27f7e2
Don Haughton <donhaughton@Dons-MacBook-Air.local>
2024-02-12 15:45:02 +0000

    moving data loading and transforming into one function

--- data_loading.py
+++ data_loading.py
@@ -1,36 +1,36 @@
-import numpy as np
 from pathlib import Path
-import matplotlib.pyplot as plt
-import math
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 
 
-def load_data(train_path: Path, test_path: Path):
-    train_df = pd.read_csv(train_path)
-    test_df = pd.read_csv(test_path)
-    return train_df, test_df
+
+def load_and_transform_data(file_path: Path, cols_to_normalise: list, features: list):
+    data_df = pd.read_csv(file_path)
+    for col in cols_to_normalise:
+        data_df = mean_normalization(data_df=data_df, col_to_normalise=col)
+    data_df = convert_sex_to_number(data_df=data_df)
+    data_df = convert_location_to_number(data_df=data_df)
+    # I'm going to keep features I think would be most relevant
+    # I'm a bit skepctical about fare as we have class, but fare would give more of an insight into how wealthy the passengers were
+    data_df.index = data_df["PassengerId"]
+    data_df = data_df.drop(columns=[col for col in data_df.columns.to_list() if col not in features])
 
 
 def convert_sex_to_number(data_df: pd.DataFrame):
+    data_df["Sex"] = data_df["Sex"].replace("female", 0)
+    data_df["Sex"] = data_df["Sex"].replace("male", 1)
+    return data_df
+
+
+def convert_location_to_number(data_df: pd.DataFrame):
     data_df["Embarked"] = data_df["Embarked"].replace("S", 1)
     data_df["Embarked"] = data_df["Embarked"].replace("C", 2)
     data_df["Embarked"] = data_df["Embarked"].replace("Q", 3)
     return data_df
 
 
-def convert_location_to_number(data_df: pd.DataFrame):
-
-    data_df["Sex"] = data_df["Sex"].replace("female", 0)
-    data_df["Sex"] = data_df["Sex"].replace("male", 1)
-    return data_df
-
-
 def mean_normalization(data_df: pd.DataFrame, col_to_normalise: str):
     col_data = data_df[col_to_normalise]
-    col_mean = col_data.mean
+    col_mean = col_data.mean()
     divider = max(col_data) - min(col_data)
     data_df[f"{col_to_normalise}_norm"] = (col_data - col_mean) / divider
     return data_df
-
-
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from pathlib import Path
-from data_loading import load_data, convert_sex_to_number, mean_normalization
+from data_loading import load_and_transform_data
 
 
 def main(train_path: Path, test_path: Path):
-    train_df, test_df = load_data(train_path=train_path, test_path=test_path)
     cols_to_normalise = ["Age", "Fare"]
-    for col in cols_to_normalise:
-        train_df = mean_normalization(data_df=train_df, col_to_normalise=col)
-        test_df = mean_normalization(data_df=test_df, col_to_normalise=col)
-    train_df = convert_sex_to_number(data_df=train_df)
-    test_df = convert_sex_to_number(data_df=test_df)
+    features = ['Survived', 'Pclass', 'Sex', 'SibSp', 'Parch', 'Embarked', 'Age_norm', 'Fare_norm']
+    train_df = load_and_transform_data(file_path=train_path, cols_to_normalise=cols_to_normalise, features=features)
+    # Not including survived as its not in the test data - thats what we're predicting for
+    test_df = load_and_transform_data(file_path=test_path, cols_to_normalise=cols_to_normalise, features=features[1:])
+    data_split_dict = train_df.to_dict(orient="split")
+    passenger_id_to_feature = {data_split_dict["index"][idx]: data_split_dict["data"][idx] for idx in range(len(data_split_dict["data"]))}
     db = 1
 
 
